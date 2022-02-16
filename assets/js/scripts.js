fccb6bf2c4f6afa0bc3279139f224b00c0166415
Cristiano Idelfonso da Silva <cristiano_idelfonso@hotmail.com>
2022-02-16 13:05:36 -0300

    Finalizando funções  javascript do jogo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,16 +7,16 @@ let score = 0;
 // 2 = amarelo
 // 3 = azul
 
-const blue = document.querySelector('.blue');
-const red = document.querySelector('.red');
 const green = document.querySelector('.green');
+const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
 
 
 // Cria ordem aleatória de cores.
 let shuffleOrder = () => {
   // Sorteia um número aleatório de 0 a 3 e faz o arredondamento
-  let colorOrder = Math.floor( Math.random * 4 );
+  let colorOrder = Math.floor( Math.random() * 4 );
   
   order[order.length] = colorOrder;
 
@@ -24,7 +24,6 @@ let shuffleOrder = () => {
 
   for(let i in order){
     let elementColor = createColorElement(order[i]);
-    
     lightColor( elementColor, Number(i) + 1 );
   }
 }
@@ -32,11 +31,11 @@ let shuffleOrder = () => {
 
 // Destaca a próxima cor.
 let lightColor = (element, number ) => {
-  let number = number * 500;
-  
+  let time = number * 500;
+
   setTimeout(() => {
     element.classList.add('selected');
-  }, number - 250);
+  }, time - 250);
 
   setTimeout(() => {
     element.classList.remove('selected');
@@ -47,8 +46,8 @@ let lightColor = (element, number ) => {
 // Checa se os botoes clicado são os mesmo da ordem gerada no jogo.
 let checkOrder = () => {
   for(let i in clickedOrder){
-    if(clickedOrder != order[i]){
-      lose();
+    if(clickedOrder[i] != order[i]){
+      gameOver();
       break;
     }
   }
@@ -66,10 +65,51 @@ let click = (color) => {
   
   setTimeout(() => {
     createColorElement(color).classList.remove('selected');
-  });
+    checkOrder();
+  }, 250);
 
-  checkOrder();
 }
 
-// 
+// 0 = green ; 1 = red ; 2 = yellow ; 3 = blue
+// Função que retorna a cor
+let createColorElement = (color) => {
+  if(color == 0) {
+    return green;
+  }else if(color == 1) {
+    return red;
+  }else if(color == 2) {
+    return yellow;
+  }else if(color == 3) {
+    return blue;
+  }
+}
 
+// Função para próximo nível do jogo
+let nextLevel = () => {
+  score++;
+  shuffleOrder();
+}
+
+// Função para game-over
+let gameOver = () => {
+  alert(`Pontuação: ${score}\nVocê perdeu!\nClique Ok para iniciar nova partida!`);
+  order = [];
+  clickedOrder = [];
+  playGame();
+}
+
+// Função de start do jogo
+let playGame = () => {
+  // alert(`Bem vindo ao Genius!!!`);
+  score = 0;
+  nextLevel();
+}
+
+// Evento de clicks para as cores
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+// Start do jogo
+playGame();
